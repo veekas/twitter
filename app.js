fc6efbc82a9a74b6a4694234bc586181fd6c55ba
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-09-18 18:32:05 -0400

    chugging along

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+// set up
 const express = require('express');
 const app = express();
+
+// logs and prettification
+const volleyball = require('volleyball');
+app.use(volleyball);
+// installed morgan to check it out, but we like volleyball while we're learning
+// var morgan = require('morgan');
+// app.use(morgan);
+const chalk = require('chalk');
 
 app.listen(3000, function () {
   console.log(`We good.`);
 });
 
+app.use((request, response, next) => {
+  console.log(chalk.green(`'throw some text in there, we'll see what happens' -kyle`));
+  next();
+});
+
 app.get('/', (request, response) => {
-  response.send(`\'some message, whatever you want it to be.\' -kyle`)
+  response.send(`'some message, whatever you want it to be.' -kyle`);
+});
+
+app.get('/news', (request, response) => {
+  response.send(`'breaking: the world is ending' -reporting from the world, kyle`);
 });
