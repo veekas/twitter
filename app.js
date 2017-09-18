464f5833450b3944b66e5b02342fa2cc1c92a037
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-09-18 19:39:05 -0400

    trying to figure out nunjucks, not currently working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // set up
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
 
 // logs and prettification
 const volleyball = require('volleyball');
@@ -9,6 +10,28 @@ app.use(volleyball);
 // var morgan = require('morgan');
 // app.use(morgan);
 const chalk = require('chalk');
+
+// const tolkeinRowlingUniverse = {
+//   title: 'An Example',
+//   people: [
+//     { name: 'Gandalf' },
+//     { name: 'Frodo' },
+//     { name: 'Hermione' }
+//   ]
+// };
+
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', tolkeinRowlingUniverse, (err, output) => {
+//   if (err) console.log('There was an error in the nunjucks.render');
+//   console.log(output);
+// });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
+app.render('index', { title: 'Hall of Fame', people: people });
 
 app.listen(3000, function () {
   console.log(`We good.`);
