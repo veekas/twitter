c882ec6265590c9a88e4344cd6926a65d18d8030
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-09-19 10:56:32 -0400

    figured out nunjucks

--- app.js
+++ app.js
@@ -11,41 +11,26 @@ app.use(volleyball);
 // app.use(morgan);
 const chalk = require('chalk');
 
-// const tolkeinRowlingUniverse = {
-//   title: 'An Example',
-//   people: [
-//     { name: 'Gandalf' },
-//     { name: 'Frodo' },
-//     { name: 'Hermione' }
-//   ]
-// };
-
-// nunjucks.configure('views', {noCache: true});
-// nunjucks.render('index.html', tolkeinRowlingUniverse, (err, output) => {
-//   if (err) console.log('There was an error in the nunjucks.render');
-//   console.log(output);
-// });
-
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
-
-const people = [{ name: 'Full' }, { name: 'Stacker' }, { name: 'Son' }];
-app.render('index', { title: 'Hall of Fame', people: people });
-
 app.listen(3000, function () {
   console.log(`We good.`);
 });
 
-app.use((request, response, next) => {
-  console.log(chalk.green(`'throw some text in there, we'll see what happens' -kyle`));
-  next();
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+const people = {
+  title: 'Fullstack Example',
+  people: [
+    { name: 'Full' },
+    { name: 'Stacker' },
+    { name: 'Son' }]
+};
+
+app.get('/', (request, res, next) => {
+  res.render('index.html', people);
 });
 
-app.get('/', (request, response) => {
-  response.send(`'some message, whatever you want it to be.' -kyle`);
-});
-
-app.get('/news', (request, response) => {
-  response.send(`'breaking: the world is ending' -reporting from the world, kyle`);
+app.get('/news', (request, res) => {
+  res.send(`'breaking: the world is ending' -reporting from the world, kyle`);
 });
